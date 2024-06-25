e21a70dccefbe438bf29fea701ec20ec51bd6a66
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-25 08:13:47 +0300

    created: patients index, show request

---
+++ front-end/src/app/api/patients/[id]/route.js
@@ -0,0 +1,17 @@
+import fetchServer from "@/lib/fetch-server";
+
+export async function GET(request, response, next) {
+    try {
+        const id = request.params.id;
+        const endpoint = `/patients/${id}`;
+        const backendResponse = await fetchServer({
+            method: "GET",
+            endpoint: endpoint,
+        });
+
+        const data = await backendResponse.json();
+        response.json(data);
+    } catch (err) {
+        next(err);
+    }
+}
---
+++ front-end/src/app/api/patients/route.js
@@ -0,0 +1,18 @@
+import fetchServer from "@/lib/fetch-server";
+import {NextResponse} from "next/server";
+
+export async function GET(request, response, next) {
+    try {
+        const {pathname} = new URL(request.url);
+        const endpoint = pathname.replace("/api/", "/");
+        const backendResponse = await fetchServer({
+            method: "GET",
+            endpoint: endpoint,
+        });
+
+        const data = await backendResponse.json();
+        return NextResponse.json(data, { status: backendResponse.status });
+    } catch (err) {
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: err.status || 500})
+    }
+}
