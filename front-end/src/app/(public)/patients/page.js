76e0951129698731e81781b080a3adbe43aeadff
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-25 20:20:28 +0300

    frontend # created: patients page

---
+++ front-end/src/app/(public)/patients/page.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+
+const Page = () => {
+    return (
+        <Fragment>
+            <div className="sm:flex sm:flex-col md:border p-6 rounded-xl">
+                <div className="sm:flex-auto">
+                    <h1 className="text-xl font-semibold dark:text-themeHoverText">Patients</h1>
+                    <p className="mt-2 text-sm dark:text-themePassiveText text-themeHover">Patients are the people who are registered in the system.</p>
+                </div>
+                <section className="my-2 flex flex-col gap-2 p-4 rounded gap-y-4">
+
+                </section>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Page;
