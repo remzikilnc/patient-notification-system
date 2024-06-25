5c07ba9a0602537f84a3e2abf2bba795cfb444e1
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-25 18:06:59 +0300

    created: logo component for modularity

---
+++ front-end/src/components/logo/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Link from "next/link";
+
+const classNames = (...classes) => {
+    return classes.filter(Boolean).join(' ');
+}
+export const Logo = ({className}) => {
+
+    return (
+        <div className={classNames("flex flex-shrink-0 items-center gap-x-2", className)}>
+            <div className="relative h-7 w-7 bg-white rounded-full flex items-center justify-center">
+                <span className="absolute h-3.5 w-3.5 rounded-full bg-primary"></span>
+            </div>
+            <Link className=" flex items-center gap-x-0.5" href="/">
+                <span className="font-semibold text-2xl leading-3 text-white">PN System</span>
+                <span className="text-xs text-white/80 mb-1">Tiga</span>
+            </Link>
+        </div>
+    );
+};
