253e5f4a353918e43047259baa24887573d93a51
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-25 18:04:10 +0300

    installed: headless ui for dropdowns, modals, etc.. & updated: tailwind config for theme colors

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#1a1f8f",
+                secondary: "#06493a",
+                passiveText: "#282828",
+                darkPassiveText: "#9ca3af",
+            }
 
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
