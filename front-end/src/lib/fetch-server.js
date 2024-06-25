668645e4e45cac921a165953f2ec3a4e34d75fef
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-25 06:22:13 +0300

    created: fetchServer method for API requests

---
+++ front-end/src/lib/fetch-server.js
@@ -0,0 +1,21 @@
+async function fetchServer({method = "GET", endpoint, body = "", headers}) {
+    try {
+        const response = await fetch(process.env.NEXT_PUBLIC_BACKEND_API_URL + endpoint.toString(), {
+            method: method,
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                ...headers,
+            },
+            body: body || undefined,
+        });
+
+        if (response.ok) {
+            return response;
+        }
+    } catch (error) {
+        throw new Error("Failed to fetch data from the server", {cause: error});
+    }
+}
+
+export default fetchServer;
