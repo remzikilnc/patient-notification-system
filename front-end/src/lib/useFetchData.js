846b2abbc2251fc9bb977c6682ce8dc1fab6ed4b
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-26 04:28:41 +0300

    frontend # updated: useFetchData hook

--- front-end/src/lib/useFetchData.js
+++ front-end/src/lib/useFetchData.js
@@ -1,32 +1,23 @@
 "use client";
 
-import React, {useCallback, useEffect, useState} from "react";
-import useSWR from 'swr'
-import fetchClient from "@/lib/fetch-client";
-import {debounce} from "lodash";
+import {useCallback, useEffect, useState} from "react";
+import useSWR from "swr";
 import {addParametersToApiURL} from "@/lib/functions/addParametersToApiURL";
+import fetchServer from "@/lib/fetch-server";
+import {debounce} from "@/lib/functions/debounce";
 
-export const fetchClientData = (endpoint, defaultFilters = {}) => {
+export const useFetchData = (endpoint, defaultFilters = {}) => {
     const [filters, setFilters] = useState(defaultFilters);
     const [data, setData] = useState(null);
     const [debouncedFilters, setDebouncedFilters] = useState(filters);
 
     const query = addParametersToApiURL(debouncedFilters);
-    const url = `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/${endpoint}${query}`;
+    const queryUrl = `/${endpoint}${query}`;
 
-    const fetcher = async url => {
-        const res = await fetchClient({method: "GET", url});
-        return await res.json();
-    };
-
-    const {
-        data: fetchedData,
-        error,
-        mutate,
-    } = useSWR(url, fetcher, {
-        revalidateOnFocus: false,
-        dedupingInterval: 32000,
-    });
+    const {data: fetchedData, error, mutate} = useSWR(queryUrl, function () {
+            fetchServer({method: "GET", endpoint: queryUrl}).then(r => r.json());
+        }
+        , {revalidateOnFocus: false, dedupingInterval: 32000});
 
     const debouncedSetFilters = useCallback(debounce(setDebouncedFilters, 300), []);
 
