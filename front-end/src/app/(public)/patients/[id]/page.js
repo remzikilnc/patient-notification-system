4e8dfc41de123ff0836b422e0a66ae8be8809027
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-26 20:00:30 +0300

    frontend # created: create & update pages

---
+++ front-end/src/app/(public)/patients/[id]/page.js
@@ -0,0 +1,18 @@
+import React from "react";
+import fetchServer from "@/lib/fetch-server";
+import PatientForm from "@/components/patient/form";
+
+async function Page({params}) {
+    const response = await fetchServer({
+        endpoint: `/patients/${params.id}`,
+    });
+
+    const model = await response.json();
+    return (
+        <section className="text-themePassiveText grid gap-y-4">
+            <PatientForm model={model} />
+        </section>
+    );
+}
+
+export default Page;
---
+++ front-end/src/app/(public)/patients/create/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PatientForm from "@/components/patient/form";
+
+async function Page() {
+    return (
+        <section className="text-themePassiveText grid gap-y-4">
+            <PatientForm/>
+        </section>
+    );
+}
+
+export default Page;
