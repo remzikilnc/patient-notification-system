99c5387076fb182f49d27f947a1ce778ae1c52a1
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-28 01:11:43 +0300

    frontend # fixed: ui bugs

--- front-end/src/components/ui/form/input/selectable/with-icon/index.js
+++ front-end/src/components/ui/form/input/selectable/with-icon/index.js
@@ -1,56 +1,61 @@
 "use client";
 
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
 import {Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions} from "@headlessui/react";
 import {PiCaretUpDownLight} from "react-icons/pi";
 import {BsCheck2} from "react-icons/bs";
+import UIFormInputError from "@/components/ui/form/input/error";
 
-export default function UIFormInputSelectableWithIcon({options = "down", data, label, selectedValue, setSelectedValue}) {
-  const [query, setQuery] = useState("");
+export default function UIFormInputSelectableWithIcon({options = "down", data, error, label, selectedValue, setSelectedValue}) {
+    const [query, setQuery] = useState("");
 
-  const filterDataItem = item => {
-    if (typeof item === "number") {
-      return item === parseInt(query, 10);
-    }
-    if (typeof item === "string") {
-      return item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""));
-    }
-    return false;
-  };
+    const filterDataItem = item => {
+        if (typeof item === "number") {
+            return item === parseInt(query, 10);
+        }
+        if (typeof item === "string") {
+            return item.toLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""));
+        }
+        return false;
+    };
 
-  const filteredData = query === "" ? data : data.filter(filterDataItem);
+    const filteredData = query === "" ? data : data.filter(filterDataItem);
 
-  return (
-    <Combobox value={selectedValue} onChange={setSelectedValue}>
-      {label && <Combobox.Label className="block text-xs font-thin text-theme dark:text-themePassiveText mb-1">{label}</Combobox.Label>}
+    return (
+        <Fragment>
+            <Combobox value={selectedValue} onChange={setSelectedValue}>
+                {label && <Combobox.Label className="block text-xs font-thin text-theme dark:text-themePassiveText mb-1">{label}</Combobox.Label>}
+                <div className="relative">
+                    <ComboboxInput
+                        className={`block w-full rounded-md  ${options === "up" ? "rounded-t-none" : ""} border h-[40px] border-gray-300 bg-white py-2 px-3 text-sm placeholder-gray-500 focus:border-primary focus:text-passiveText focus:outline-none focus:ring-0 sm:text-sm`}
+                        onChange={event => setQuery(event.target.value)}
+                        displayValue={item => item}
+                    />
+                    <ComboboxButton
+                        className="absolute inset-y-0 right-0 group flex items-center rounded-r-md px-2 focus:outline-none">
+                        <PiCaretUpDownLight className="h-5 w-5 text-gray-400 " aria-hidden="true"/>
+                    </ComboboxButton>
 
-      <div className="relative">
-        <ComboboxInput
-          className={`block w-full rounded-md  ${options === "up" ? "rounded-t-none" : ""} border border-gray-300 bg-white py-2 px-3 text-sm placeholder-gray-500 focus:border-primary focus:text-passiveText focus:outline-none focus:ring-0 sm:text-sm`}
-          onChange={event => setQuery(event.target.value)}
-          displayValue={item => item}
-        />
-        <ComboboxButton  className="absolute inset-y-0 right-0 group flex items-center rounded-r-md px-2 focus:outline-none">
-          <PiCaretUpDownLight className="h-5 w-5 text-gray-400 " aria-hidden="true" />
-        </ComboboxButton >
-
-        {filteredData.length > 0 && (
-          <ComboboxOptions className={`absolute z-10 ${options === "up" ? "bottom-full" : ""} group mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm`}>
-            {filteredData.map((item, index) => (
-                <ComboboxOption
-                    key={index}
-                    value={item}
-                    className="group flex justify-between cursor-default select-none items-center gap-2 py-1.5 px-3 data-[focus]:bg-primary data-[selected]:bg-primary data-[selected]:text-white data-[focus]:text-white"
-                >
-                  <span>{item}</span>
-                  <BsCheck2 className="invisible size-5 fill-white group-data-[selected]:visible" />
-                </ComboboxOption>
-            ))}
-          </ComboboxOptions>
-        )}
-      </div>
-    </Combobox>
-  );
+                    {filteredData.length > 0 && (
+                        <ComboboxOptions
+                            className={`absolute z-10 ${options === "up" ? "bottom-full" : ""} group mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm`}>
+                            {filteredData.map((item, index) => (
+                                <ComboboxOption
+                                    key={index}
+                                    value={item}
+                                    className="group flex justify-between cursor-default select-none items-center gap-2 py-1.5 px-3 data-[focus]:bg-primary data-[selected]:bg-primary data-[selected]:text-white data-[focus]:text-white"
+                                >
+                                    <span>{item}</span>
+                                    <BsCheck2 className="invisible size-5 fill-white group-data-[selected]:visible"/>
+                                </ComboboxOption>
+                            ))}
+                        </ComboboxOptions>
+                    )}
+                </div>
+            </Combobox>
+            {error && <UIFormInputError className="mt-2" message={error}/>}
+        </Fragment>
+    );
 }
 
 
--- front-end/src/components/ui/link/button/index.js
+++ front-end/src/components/ui/link/button/index.js
@@ -4,7 +4,7 @@ import {classNames} from "@/lib/functions/classNames";
 
 export const UILinkButton = ({subMenu, activePathName, className, showDefaultIcon}) => {
     const linkClassName = classNames(
-        activePathName === subMenu.activePathName ? "bg-white text-secondary" : "text-white hover:text-secondary transition-all delay-75 ",
+        activePathName === subMenu.activePathName ? "bg-white text-primary" : "text-white hover:text-primary transition-all delay-75 ",
         "group relative rounded-md py-1.5 px-2 flex items-center text-xs font-medium hover:bg-white",
         className
     );
@@ -14,7 +14,7 @@ export const UILinkButton = ({subMenu, activePathName, className, showDefaultIco
             {subMenu.icon ? (
                 <span className="relative z-10">{subMenu.icon}</span>
             ) : showDefaultIcon ? (
-                <span className={classNames(activePathName === subMenu.activePathName ? "bg-primary" : "bg-white", "mr-3 block transition-all z-10 group-hover:bg-themePrimary duration-200 h-[1px] w-3")}></span>
+                <span className={classNames(activePathName === subMenu.activePathName ? "bg-primary" : "bg-white", "mr-3 block transition-all z-10 group-hover:bg-primary duration-200 h-[1px] w-4")}></span>
             ) : null}
             <span className="relative z-10">{subMenu.title}</span>
         </Link>
