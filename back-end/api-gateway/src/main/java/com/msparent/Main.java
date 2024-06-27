c86293c424cab80b7286ee816c19af1a5eaf0fa0
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-27 20:30:05 +0300

    backend # created: api gateway

---
+++ back-end/api-gateway/src/main/java/com/msparent/Main.java
@@ -0,0 +1,13 @@
+package com.msparent;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
