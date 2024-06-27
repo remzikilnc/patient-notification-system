f25a617b2498f92447deff28a108346985db8d17
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-27 22:21:15 +0300

    backend # created: api-gateway service

--- back-end/api-gateway/src/main/java/com/msparent/ApiGatewayApplication.java
+++ back-end/api-gateway/src/main/java/com/msparent/ApiGatewayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class ApiGatewayApplication  {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
