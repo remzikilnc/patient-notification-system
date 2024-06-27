68c7c414a7eab07e4eab92943076c512d19d336b
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-28 01:08:14 +0300

    frontend # updated: sidebar navigations

--- front-end/src/app/(public)/layout.js
+++ front-end/src/app/(public)/layout.js
@@ -2,15 +2,20 @@ import React, {Fragment} from 'react';
 import LeftSidebar from "@/components/left-sidebar";
 import LeftSidebarNavigations from "@/components/left-sidebar/navigations";
 import LeftSidebarMobile from "@/components/left-sidebar/mobile";
+import {GoRelFilePath} from "react-icons/go";
+import {RiHome3Line} from "react-icons/ri";
+import {MdSpaceDashboard} from "react-icons/md";
 
 const navigations = [
     {
         title: "Dashboard",
         href: "/",
         activePathName: "/",
+        icon: <MdSpaceDashboard   className="flex-shrink-0 h-6 w-6 mr-1"/>,
     },
     {
         title: "Patients",
+        icon: <GoRelFilePath className="flex-shrink-0 h-6 w-6"/>,
         subMenu: [
             {
                 title: "All Patients",
@@ -21,6 +26,40 @@ const navigations = [
                 title: "New Patient",
                 href: "/patients/create",
                 activePathName: "/patients/create",
+            },
+        ],
+    },
+    {
+        title: "Notifications",
+        icon: <GoRelFilePath className="flex-shrink-0 h-6 w-6"/>,
+        subMenu: [
+            {
+                subMenuTitle: "Templates",
+                listClassName: "my-2",
+            },
+            {
+                title: "All Templates",
+                href: "/navigation",
+                activePathName: "/navigation",
+            },
+            {
+                title: "New Template",
+                href: "/navigation/template",
+                activePathName: "/navigation",
+            },
+            {
+                subMenuTitle: "Targets",
+                listClassName: "my-2",
+            },
+            {
+                title: "All Targets",
+                href: "/navigation/targets",
+                activePathName: "/navigation",
+            },
+            {
+                title: "New Target",
+                href: "/navigation/targets/create",
+                activePathName: "/navigation",
             },
         ],
     },
--- front-end/src/components/left-sidebar/navigations/index.js
+++ front-end/src/components/left-sidebar/navigations/index.js
@@ -6,6 +6,7 @@ import {Disclosure, DisclosureButton, DisclosurePanel} from "@headlessui/react";
 import {usePathname} from "next/navigation";
 import {UILinkButton} from "@/components/ui/link/button";
 import {classNames} from "@/lib/functions/classNames";
+import {FiChevronDown} from "react-icons/fi";
 
 
 export default function LeftSidebarNavigations({navigations}) {
@@ -21,10 +22,9 @@ export default function LeftSidebarNavigations({navigations}) {
             {navigations.map((navigation, index) => (
                 <Fragment key={"nav-group-" + index}>
                     {navigation.subMenu && navigation.subMenu.length > 0 ? (
-                        <Disclosure as="div" className="mt-1 first:mt-0"
-                                    defaultOpen={isAnySubMenuActive(navigation.subMenu)}>
+                        <Disclosure as="div" className="mt-1 first:mt-0" defaultOpen={isAnySubMenuActive(navigation.subMenu)}>
                             <DisclosureButton
-                                className={`text-xs p-3 font-semibold group flex w-full items-center justify-between !outline-0 rounded ${
+                                className={`text-xs p-3 font-semibold group flex w-full items-center justify-between data-[open]:bg-white data-[open]:text-primary !outline-0 rounded-md max-h-10 ${
                                     isAnySubMenuActive(navigation.subMenu) ? "bg-white text-primary" : "text-white hover:text-primary hover:bg-white transition-all delay-75"
                                 }`}
                                 aria-label={navigation.title}
@@ -33,13 +33,12 @@ export default function LeftSidebarNavigations({navigations}) {
                                     {navigation.icon ? <span className="relative z-10">{navigation.icon}</span> : null}
                                     <span className="relative z-10">{navigation.title}</span>
                                 </div>
-                                <span className="h-4 w-4 group-data-[open]:rotate-180 relative z-10"> ` </span>
+                                <FiChevronDown className="h-4 w-4 group-data-[open]:rotate-180 relative " />
                             </DisclosureButton>
-                            <DisclosurePanel className="mt-0.5 text-sm/5 text-white/50 ml-4">
+                            <DisclosurePanel className="mt-1 text-sm/5 text-white/50 ml-4">
                                 {navigation.subMenu.map((subMenu, subIndex) => (
-                                    <li key={"sub-nav-" + subIndex}
-                                        className={classNames(subIndex > 0 ? `my-0.5 ${subMenu.listClassName ?? ""}` : `${subMenu.listClassName ?? ""}`)}>
-                                        <UILinkButton showDefaultIcon className="!p-3 max-h-10" subMenu={subMenu} activePathName={activePathName}/>
+                                    <li key={"sub-nav-" + subIndex} className={classNames(subIndex > 0 ? `my-0.5 ${subMenu.listClassName ?? ""}` : `${subMenu.listClassName ?? ""}`)}>
+                                        {subMenu.subMenuTitle ? <span className="text-xs font-semibold text-white">{subMenu.subMenuTitle}</span> : <UILinkButton showDefaultIcon className="!p-2.5 max-h-10" subMenu={subMenu} activePathName={activePathName}/>}
                                     </li>
                                 ))}
                             </DisclosurePanel>
@@ -47,7 +46,7 @@ export default function LeftSidebarNavigations({navigations}) {
                     ) : (
                         <li className="mt-1">
                             <Link href={navigation.href}
-                                  className={classNames(activePathName === navigation.activePathName ? "bg-white text-primary" : "hover:bg-white text-white hover:text-primary transition-all delay-75", "rounded text-xs p-3 font-semibold group flex w-full items-center !outline-0")}>
+                                  className={classNames(activePathName === navigation.activePathName ? "bg-white text-primary" : "hover:bg-white text-white hover:text-primary transition-all delay-75", "max-h-10 rounded-md text-xs p-3 font-semibold group flex w-full items-center !outline-0")}>
                                 {navigation.icon ? <span className="relative z-10">{navigation.icon}</span> : null}
                                 <span className="relative z-10">{navigation.title}</span>
                             </Link>
