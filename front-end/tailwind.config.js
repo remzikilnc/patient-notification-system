78f7f3b9efd90a09ae18be3b97d1a2c949373d1e
Remzi Kılınç <remzikilnc46@gmail.com>
2024-06-28 17:37:59 +0300

    frontend # updated: prettier config, tailwind config, next config

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,31 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-import forms from "@tailwindcss/forms";
+import forms from '@tailwindcss/forms';
 module.exports = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
+    content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
     theme: {
         extend: {
             colors: {
-                body: "#eef1f9",
+                body: '#eef1f9',
                 /*Theme*/
-                primary: "#1a1f8f",
-                secondary: "#1f26be",
-                tertiary: "#077ac2",
+                primary: '#1a1f8f',
+                secondary: '#1f26be',
+                tertiary: '#077ac2',
 
                 /*Text*/
-                passiveText: "#282828",
-                default: "#131313",
-                activeText: "#070707",
-                darkPassiveText: "#9ca3af",
+                passiveText: '#282828',
+                default: '#131313',
+                activeText: '#070707',
+                darkPassiveText: '#9ca3af',
 
                 /*Border*/
-                primaryBorder: "#252a98",
-                passiveBorder: "#dedede",
-            }
-
+                primaryBorder: '#252a98',
+                passiveBorder: '#dedede',
+            },
         },
     },
     plugins: [forms],
